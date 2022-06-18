2a0ecd17960586c94421a2ecd78b25380c4ddac2
Saurabh Parate <sparate04@gmail.com>
2022-06-18 15:51:26 +0530

    Working on image upload

--- app/src/js/app.js
+++ app/src/js/app.js
@@ -8,7 +8,7 @@
 
 var web3;
 
-var agentContractAddress = '0x75E115394aacC7c6063E593B9292CB9417E4cbeC';
+var agentContractAddress = '0xa043881Da7eC6627F4CB0ea599d59A3fd56f45Ac';
 
 function connect(){
     web3 = new Web3(window.ethereum)
